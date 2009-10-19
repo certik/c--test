f794b941e808c41a07173748ecb547da95318894
Ondrej Certik <ondrej@certik.cz>
2009-10-19 01:30:23 -0700

    sum0 added

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <vector>
 #include <typeinfo>
 
+#define N 100
+
 std::vector<double> & init_vector(int n)
 {
     std::vector<double> &v = *(new std::vector<double>(n));
@@ -15,28 +17,44 @@ std::vector<double> & init_vector(int n)
     return v;
 }
 
+double sum0(double *v, int n)
+{
+    double sum = 0;
+    for (int k=0; k < N; k++) {
+        for (int j=0; j < n; j++)
+            sum += v[j];
+    }
+    return sum;
+}
+
 double sum1(std::vector<double> &v)
 {
     double sum = 0;
-    for (int j=0; j < v.size(); j++)
-        sum += v[j];
+    for (int k=0; k < N; k++) {
+        for (int j=0; j < v.size(); j++)
+            sum += v[j];
+    }
     return sum;
 }
 
 double sum2(std::vector<double> &v)
 {
     double sum = 0;
-    for (std::vector<double>::const_iterator it=v.begin();
-            it != v.end(); ++it)
-        sum += *it;
+    for (int k=0; k < N; k++) {
+        for (std::vector<double>::const_iterator it=v.begin();
+                it != v.end(); ++it)
+            sum += *it;
+    }
     return sum;
 }
 
 double sum3(std::vector<double> &v)
 {
     double sum = 0;
-    for (auto it=v.begin(); it != v.end(); ++it)
-        sum += *it;
+    for (int k=0; k < N; k++) {
+        for (auto it=v.begin(); it != v.end(); ++it)
+            sum += *it;
+    }
     return sum;
 }
 
@@ -46,17 +64,24 @@ double sum3(std::vector<double> &v)
 double sum4(std::vector<double> &v)
 {
     double sum = 0;
-    for_each(x, v)
-        sum += x;
+    for (int k=0; k < N; k++) {
+        for_each(x, v)
+            sum += x;
+    }
     return sum;
 }
 
 int main()
 {
-    std::vector<double> &v = init_vector(11000000);
+    int n = 1e7;
+    printf("initializing v...\n");
+    std::vector<double> &v = init_vector(n);
+    double *a = &(v[0]);
     clock_t t;
-    auto i = 5;
-    printf("i=%d\n", i);
+    printf("done.\n");
+    t = clock();
+    printf("sum0: %f\n", sum0(a, n));
+    printf("time: %d\n", clock() - t);
     t = clock();
     printf("sum1: %f\n", sum1(v));
     printf("time: %d\n", clock() - t);
